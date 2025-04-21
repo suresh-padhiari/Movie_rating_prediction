17c3a20830557ca8f569c5fcd478311b9f1cd827
suresh-padhiari <suresh.padhiari@gmail.com>
2025-04-21 15:47:22 -0400

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.title("My first app")
-name = st.input("What is your name?")
+name = st.text_input("What is your name?")
 if name:
-  st.write(f"Hello, {name}")
+st.write(f"Hello, {name}!")
+
